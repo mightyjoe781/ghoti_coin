6af703ecfa37741d10977bf1a92459aefeb73306
smk <sudhanshumohan781@gmail.com>
2025-11-13 11:10:54 +0530

    enable only one coin for now

--- init.lua
+++ init.lua
@@ -1,17 +1,21 @@
-core.register_craftitem("ghoti_coin:bronze_coin", {
-    description = "Bronze Coin",
-    inventory_image = "bronze_coin.png",
-    stack_max = 1000
-})
+-- TODO: make this more dynamic and allow loading from settings
 
-core.register_craftitem("ghoti_coin:silver_coin", {
-    description = "Silver Coin",
-    inventory_image = "silver_coin.png",
-    stack_max = 500
-})
+-- core.register_craftitem("ghoti_coin:bronze_coin", {
+--     description = "Bronze Coin",
+--     inventory_image = "bronze_coin.png",
+--     stack_max = 1000
+-- })
+
+-- core.register_craftitem("ghoti_coin:silver_coin", {
+--     description = "Silver Coin",
+--     inventory_image = "silver_coin.png",
+--     stack_max = 500
+-- })
 
 core.register_craftitem("ghoti_coin:gold_coin", {
     description = "Gold Coin",
     inventory_image = "gold_coin.png",
     stack_max = 100
 })
+
+print("[OK] Ghoti Coin Loaded!...")
