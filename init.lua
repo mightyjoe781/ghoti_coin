d561b399831c7bf1e57608861225d8def3e36d28
smk <sudhanshumohan781@gmail.com>
2025-11-13 10:51:36 +0530

    feat: initial commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+core.register_craftitem("ghoti_coin:bronze_coin", {
+    description = "Bronze Coin",
+    inventory_image = "bronze_coin.png",
+    stack_max = 1000
+})
+
+core.register_craftitem("ghoti_coin:silver_coin", {
+    description = "Silver Coin",
+    inventory_image = "silver_coin.png",
+    stack_max = 500
+})
+
+core.register_craftitem("ghoti_coin:gold_coin", {
+    description = "Gold Coin",
+    inventory_image = "gold_coin.png",
+    stack_max = 100
+})
